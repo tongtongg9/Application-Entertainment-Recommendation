75549dc84d465b91d1e8e87c452dfe99b4155551
tongtongg9 <kampon.lamyai@gmail.com>
2021-08-21 15:49:48 +0700

    v--210821

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:hexcolor/hexcolor.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
-import 'package:my_finalapp1/MainPageOwner.dart';
-import 'package:my_finalapp1/MainPageUser.dart';
-import 'package:my_finalapp1/owner/ShowDetailNP_owner.dart';
-import 'package:my_finalapp1/sheetBottom.dart';
-import 'package:my_finalapp1/np/add_Imgs_np.dart';
-import 'package:my_finalapp1/user/Page/Home/DetailNp.dart';
-import 'package:my_finalapp1/user/Page/Home/Review_np_list.dart';
-import 'package:my_finalapp1/user/Page/Home/Review_user.dart';
-import 'package:my_finalapp1/user/bottom_nav_bar.dart';
-import 'package:my_finalapp1/MainPage.dart';
-import 'package:my_finalapp1/NewHome.dart';
-import 'package:my_finalapp1/user/Page/Profile/Profile.dart';
-import 'package:my_finalapp1/np/AddNP.dart';
-import 'package:my_finalapp1/np/RegiterPageNP.dart';
-import 'package:my_finalapp1/owner/Dashboard.dart';
-import 'package:my_finalapp1/owner/EditPageOw.dart';
-import 'package:my_finalapp1/owner/MyPubOw.dart';
-import 'package:my_finalapp1/owner/ShowDataOw.dart';
-import 'package:my_finalapp1/uploadPicture.dart';
-import 'package:my_finalapp1/user/Page/Profile/EditPageUser.dart';
-import 'package:my_finalapp1/user/LoginPage.dart';
-import 'package:my_finalapp1/owner/LoginPageOw.dart';
-import 'package:my_finalapp1/user/LoginUser.dart';
-import 'package:my_finalapp1/user/RegisUser.dart';
-import 'package:my_finalapp1/user/RegisterPage.dart';
-import 'package:my_finalapp1/owner/RegisterPageOw.dart';
-import 'package:my_finalapp1/user/Page/Profile/ShowDataUser.dart';
-import 'package:my_finalapp1/user/ShowDetailNp.dart';
+import 'package:my_finalapp1/routes/routes.dart';
 import 'package:my_finalapp1/widget/colors.dart';
 
 void main() {
@@ -41,46 +12,16 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  // get initialRoute => Routes();
+  // get routes => Routes();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: mytheme(),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => MainPage(),
-        '/mainpageuser': (context) => MainPageUser(),
-        '/mainpageowner': (context) => MainPageOwner(),
-        '/uloginpage': (context) => LoginUser(),
-        // '/uloginpage': (context) => LoginPage(),
-        // '/uregisterpage': (context) => RegisterPage(),
-        '/uregisterpage': (context) => RegisUser(),
-        '/homepage': (context) => BottomNavBar(),
-        // '/showdetailnp': (context) => ShowDetailNp(),
-        '/showdetailnp': (context) => DetailNp(),
-        '/reviewpage': (context) => ReviewPage(),
-        '/reviewlistnp': (context) => ReviewListNp(),
-        '/uprofilescreen': (context) => Profile(),
-        '/upload': (context) => UploadPicture(),
-        '/showdataUser': (context) => ShowDataUser(),
-        '/editdataUser': (context) => EditPageUser(),
-
-        // '/owloginpage' : (context) => LoginPageOw(),
-
-        //  '/': (context) => LoginPageOw(),
-
-        '/owloginpage': (context) => LoginPageOw(),
-        '/owregisterpage': (context) => RegisterPageOw(),
-        '/owdashboard': (context) => Dashboard(),
-        '/owshowdata': (context) => ShowDataOw(),
-        '/oweditdata': (context) => EditPageOw(),
-        '/owmypub': (context) => MyPubOw(),
-        '/showdetailnpowner': (context) => ShowDetailNPowner(),
-        // '/owaddmypub' : (context) => RegisterPageOw(),
-        '/owaddmypub': (context) => AddNP(),
-        '/addimgsnp': (context) => AddImagesNP(),
-      },
+      initialRoute: '/main',
+      routes: routes,
     );
   }
 
